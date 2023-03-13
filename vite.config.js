6356de6ce9068609b6ebde3fa83d1425249b5222
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2023-03-13 12:03:59 +0100

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/https://github.com/chasacademy-sandra-larsson/vite-react-deploy/',
+  base: "/vite-react-deploy/",
   plugins: [react()],
 })
